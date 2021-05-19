76f60c63b6fa9751c74c5cbc6d2ed3529935e2ab
Nanlung <nanlung.temlong@gmail.com>
2021-05-20 00:04:19 +0100

    App.js modified

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -4,13 +4,19 @@ const tasks = [
     text: "Volley Ball game",
     day: "Thursday, May 6 2021",
     reminder: true
+  },
+  {
+    id: 2,
+    text: "Community Meeting",
+    day: "Friday, May 7 2021",
+    reminder: true
   }
 ]
 
 const Tasks = () => {
   return (
     <div>
-      
+      {tasks.map((task) =>(<h3>{task.text}</h3>) ) }
     </div>
   )
 }
