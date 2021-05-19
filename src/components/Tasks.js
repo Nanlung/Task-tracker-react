01389bf01a3b93cb22380fc12feed106adccc336
Nanlung <nanlung.temlong@gmail.com>
2021-05-19 23:56:42 +0100

    tasks component

---
+++ src/components/Tasks.js
@@ -0,0 +1,18 @@
+const tasks = [
+  {
+    id: 1,
+    text: "Volley Ball game",
+    day: "Thursday, May 6 2021",
+    reminder: true
+  }
+]
+
+const Tasks = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Tasks
