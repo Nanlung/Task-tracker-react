cce4f68bc1dd0c965713179e5b7b2773476d8208
Nanlung <nanlung.temlong@gmail.com>
2021-05-20 23:33:40 +0100

    tasks modification

--- src/App.js
+++ src/App.js
@@ -2,10 +2,28 @@ import Header from './components/Header'
 import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks, setTasks] = useState(
+    [
+  {
+    id: 1,
+    text: "Volley Ball game",
+    day: "Thursday, May 6 2021",
+    reminder: true
+  },
+  {
+    id: 2,
+    text: "Community Meeting",
+    day: "Friday, May 7 2021",
+    reminder: true
+  }
+]
+
+  )
+
   return (
     <div className="App">
       <Header title="Task Tracker" />
-      <Tasks />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -2,26 +2,9 @@ import { useState} from 'react'
 
 
 
-const Tasks = () => {
+const Tasks = ({tasks}) => {
 
-  const [tasks, setTasks] = useState(
-    [
-  {
-    id: 1,
-    text: "Volley Ball game",
-    day: "Thursday, May 6 2021",
-    reminder: true
-  },
-  {
-    id: 2,
-    text: "Community Meeting",
-    day: "Friday, May 7 2021",
-    reminder: true
-  }
-]
-
-  )
-
+ 
   return (
     <div>
       {tasks.map((task) =>(<h3 key={task.id}>  {task.text}</h3>) ) }
