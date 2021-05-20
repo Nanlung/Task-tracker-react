d5d8d6a32f23d1b1544f30d00051485862add465
Nanlung <nanlung.temlong@gmail.com>
2021-05-21 00:16:43 +0100

    delete task function

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -2,12 +2,12 @@ import { useState} from 'react'
 
 import Task from './Task';
 
-const Tasks = ({tasks}) => {
+const Tasks = ({tasks, onDelete}) => {
 
   return (
     <div>
       {tasks.map((task) =>(
-      <Task key={task.id}  task={task} />) ) }
+      <Task key={task.id}  task={task} onDelete={onDelete} />) ) }
     </div>
   )
 }
